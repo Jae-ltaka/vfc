5e5e476cebb2f072ca6772ad53ae856eac69817d
Pascal190 <115409615+Pascal190@users.noreply.github.com>
2025-04-04 13:18:24 +0200

    Erreur

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
-
+import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { ArticlesComponent } from './components/articles/articles.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
 @NgModule({
   declarations: [
+    HeaderComponent,
+    BannerComponent,
+    ArticlesComponent,
+    ContactFormComponent,
+    FooterComponent,
   ],
 })
 export class AppModule { }
