089f58e5b0fc72b1665e9d81dea0b47ecd78fc2b
Pascal190 <115409615+Pascal190@users.noreply.github.com>
2025-05-19 16:46:25 +0200

    articles

--- src/app/components/articles/articles.component.ts
+++ src/app/components/articles/articles.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule }   from '@angular/forms';
+
 
 @Component({
   selector: 'app-articles',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './articles.component.html',
   styleUrl: './articles.component.scss'
 })
 export class ArticlesComponent {
-
 }
--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -2,18 +2,44 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../components/header/header.component';
 import { FooterComponent } from '../components/footer/footer.component';
 import { Router } from '@angular/router';
+import { NgFor } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule }   from '@angular/forms';
+
+interface Article {
+  title: string;
+  imageUrl: string;
+}
 
 @Component({
   selector: 'app-questions',
   standalone: true,
-  imports: [HeaderComponent,FooterComponent],
+  imports: [HeaderComponent,FooterComponent,NgFor,CommonModule,FormsModule],
   templateUrl: './questions.component.html',
   styleUrl: './questions.component.scss'
 })
 export class QuestionsComponent {
   constructor(private router: Router) {}
-  
-    goDon(): void {
-      this.router.navigate(['/ambassadeurs']);
-    }
+  searchTerm: string = '';
+  articles: Article[] = [
+    {
+      title: 'Jésus avait-il un ange gardien ?',
+      imageUrl: 'assets/ange.jpg'
+    },
+    {
+      title: 'Faut-il lire la Bible de manière littérale ou symbolique ?',
+      imageUrl: 'assets/bible.jpg'
+    },
+    {
+      title: 'Les péchés mortels peuvent-ils être pardonnés sans être confessés ?',
+      imageUrl: 'assets/main.jpg'
+    },
+    {
+      title: 'Jésus est-il mort uniquement pour les chrétiens ?',
+      imageUrl: 'assets/jesus.jpg'
+    },
+  ];
 }
+   
+
